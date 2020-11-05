83699dd560507f420cbe6137412dc127e2df0cb6
Arm <armoop01@gmail.com>
2020-11-05 13:51:08 +0000

    1.1.1.3

--- main.c
+++ main.c
@@ -1,27 +1,32 @@
 #include <stdio.h>
-void Myfunc(){
-  float power,x,result = 1;
-  //int  power;
+float Myfunc(){
+  float  power, x, result = 1;
+  //int ;
 
     printf("Enter basenumber: ");
     scanf("%f", &x);
 
     printf("Enter powernumber: ");
     scanf("%f", &power); 
-    int b = power;
+    int d = power;
     int i = 1;
-  if(power != b || power < 0){
-      printf("Can't Do");
+    if(power != d || power < 0){
+      power = -1;
+      printf("Exit Program");
+      return 0 ;
     }
-    while(i <= power)
+   while(i <= power)
     {
-        result *= x;
+        result *= power;
+        printf("%.3f^%d = %.3f",x, power, result);
         i++;
+        return result ;
     }
     printf("%.3f^%d = %.3f",x, power, result);
 }
 int main(void) {
 
   Myfunc();
+  
   return 0;
 }
